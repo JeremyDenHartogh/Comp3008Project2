08cc8ea09642dbbdd0c0de9bf2100474eea17998
Shaan Malik <shaanmalik@cmail.carleton.ca>
2019-03-23 15:03:47 -0400

    added color to graphs

--- Part1/3008part1script.R
+++ Part1/3008part1script.R
@@ -4,19 +4,19 @@ tab = read.csv("data.csv", header = TRUE)
 imageTable <- tab[tab$Scheme == "Image21", ]
 textTable <- tab[tab$Scheme == "text21", ]
 #create histograms for login attempts, for each password scheme
-imageTotalHistogram = hist(imageTable$NumLogins,main="Total Login Attempts Per User: Image21",xlab="# of Attempts", breaks=20)
-textTotalHistogram = hist(textTable$NumLogins,main="Total Login Attempts Per User: text21",xlab="# of Attempts", breaks=20)
-imageSuccessfulHistogram = hist(imageTable$NumSuccessful,main="Successful Login Attempts Per User: Image21",xlab="# of Attempts", breaks=20)
-textSuccessfulHistogram = hist(textTable$NumSuccessful,main="Successful Login Attempts Per User: text21",xlab="# of Attempts", breaks=20)
-imageFailedHistogram = hist(imageTable$NumFailed,main="Failed Login Attempts Per User: Image21", xlab="# of Attempts",breaks=20)
-textFailedHistogram = hist(textTable$NumFailed,main="Failed Login Attempts Per User: text21", xlab="# of Attempts",breaks=20)
+imageTotalHistogram = hist(imageTable$NumLogins,main="Total Login Attempts Per User: Image21",xlab="# of Attempts", breaks=20, col="purple")
+textTotalHistogram = hist(textTable$NumLogins,main="Total Login Attempts: text21",xlab="# of Attempts", breaks=20, col="purple")
+imageSuccessfulHistogram = hist(imageTable$NumSuccessful,main="Successful Login Attempts: Image21",xlab="# of Attempts", breaks=20, col="light blue")
+textSuccessfulHistogram = hist(textTable$NumSuccessful,main="Successful Login Attempts: text21",xlab="# of Attempts", breaks=20, col="light blue")
+imageFailedHistogram = hist(imageTable$NumFailed,main="Failed Login Attempts: Image21", xlab="# of Attempts",breaks=20, col="red")
+textFailedHistogram = hist(textTable$NumFailed,main="Failed Login Attempts: text21", xlab="# of Attempts",breaks=20, col="red")
 #create histograms for login time for each password scheme
-imageSuccessTimeHistogram = hist(imageTable$SuccessTime,main="Average Successful Login Time Per User: Image21",xlab="Time (s)", breaks=20)      
-textSuccessTimeHistogram = hist(textTable$SuccessTime,main="Average Successful Login Time Per User: text21",xlab="Time (s)", breaks=20)
-imageFailedTimeHistogram = hist(imageTable$FailedTime,main="Average Failed Login Time Per User: Image21",xlab="Time (s)", breaks=20)
-textFailedTimeHistogram = hist(textTable$FailedTime, main="Average Failed Login Time Per User: text21",xlab="Time (s)",breaks=20)
+imageSuccessTimeHistogram = hist(imageTable$SuccessTime,main="Frequency of Average Successful Login Time: Image21",xlab="Time (s)", breaks=20, col="light blue")      
+textSuccessTimeHistogram = hist(textTable$SuccessTime,main="Frequency of Average Successful Login Time: text21",xlab="Time (s)", breaks=20, col="light blue")
+imageFailedTimeHistogram = hist(imageTable$FailedTime,main="Frequency of Average Failed Login Time: Image21",xlab="Time (s)", breaks=20, col="red")
+textFailedTimeHistogram = hist(textTable$FailedTime, main="Frequency of Average Failed Login Time: text21",xlab="Time (s)",breaks=20, col="red")
 #create boxplots for login time, with both password schemes on the same graph.
-groupedSuccessBoxplot <- boxplot(tab$SuccessTime ~ tab$Scheme, main="Successful Login Time", names=c("Image21", "Text21"), xlab="Login Scheme", ylab="Time (s)")
-groupedFailedBoxplot <- boxplot(tab$FailedTime ~ tab$Scheme, main="Failed Login Time", names=c("Image21", "text21"), xlab="Login Scheme",ylab="Time (s)")
+groupedSuccessBoxplot <- boxplot(tab$SuccessTime ~ tab$Scheme, main="Successful Login Time", names=c("Image21", "Text21"), xlab="Login Scheme", ylab="Time (s)", col="light blue")
+groupedFailedBoxplot <- boxplot(tab$FailedTime ~ tab$Scheme, main="Failed Login Time", names=c("Image21", "text21"), xlab="Login Scheme",ylab="Time (s)", col="red")
 
 
